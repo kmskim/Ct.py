9e2de2dc7665ef3399614604e8a406ff1f4a3d3a
kmskim <mnai2000@naver.com>
2026-07-13 17:48:09 +0900

    2026-07-13 hash

---
+++ 03_10_Hash.py
@@ -0,0 +1,20 @@
+class Dict:
+    def __init__(self):
+        self.items = [None] * 8
+
+    def put(self, key, value):
+        # 구현해보세요!
+        index = hash(key) %len(self.items)
+        self.items[index] = value
+        return 0;
+
+    def get(self, key):
+        index = hash(key) %len(self.items)
+        return self.items[index]
+
+my_dict = Dict()
+my_dict.put("test", 3)
+print(my_dict.get("test"))  # 3이 반환되어야 합니다!
+
+
+
